98c0fbf67426f1c920646ed7a9bdbd8f8843acf1
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2020-09-05 18:33:38 +0100

    added babel runtime plugin

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
             "@babel/preset-react",
           ],
           plugins: [
+            "@babel/plugin-transform-runtime",
             "@babel/plugin-syntax-dynamic-import",
             "@babel/plugin-proposal-export-default-from",
             "@babel/plugin-proposal-class-properties",
